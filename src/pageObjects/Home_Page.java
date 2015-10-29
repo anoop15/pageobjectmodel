0bf0235e66c006266e536d975e2a63acd6936f1d
anoop <anu.lovi@gmail.com>
2015-10-29 14:08:41 +0530

    pageobject model

---
+++ src/pageObjects/Home_Page.java
@@ -0,0 +1,42 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+
+public class Home_Page {
+
+	private static WebElement element = null;
+
+	// Driver is being passed as an Argument so that Selenium is able to locate
+	// the element on the browser (driver).
+
+	// Element is returned, so that an Action can be performed on it.
+
+	// Method is declared as Public Static, so that it can be called in any
+	// other method without instantiate the class.
+
+	public static WebElement lnk_MyAccount(WebDriver driver) {
+
+		element = driver.findElement(By.className("account_icon"));
+
+		return element;
+
+	}
+
+	public static WebElement lnk_LogOut(WebDriver driver) {
+
+		element = driver.findElement(By.id("account_logout"));
+
+		return element;
+
+	}
+
+}
+
+
+
+
+
